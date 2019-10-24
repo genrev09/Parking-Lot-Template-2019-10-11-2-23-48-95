5c4764b9472d3ed89eca99f1732452ea19579a0f
GENREV ARAMBULO (SAPE-MIS-ISD-OOCLL/MNL) <ARAMBGE@OOCL.COM>
2019-10-24 11:31:26 +0800

    - Added test for Package Service - Added another method to get all package in package service

--- src/main/java/Minipackage/Service/PackageService.java
+++ src/main/java/Minipackage/Service/PackageService.java
@@ -5,6 +5,8 @@ import Minipackage.Repository.PackageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PackageService {
     @Autowired
@@ -12,5 +14,9 @@ public class PackageService {
 
     public Package createPackage(Package myPackage) {
         return packageRepository.save(myPackage);
+    }
+
+    public List<Package> getAllPackage() {
+        return packageRepository.findAll();
     }
 }
